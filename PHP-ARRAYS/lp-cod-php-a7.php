14e1491cc47337cc8956963e4f42eb6a71bf6a3d
Lucas <lucas.rocha9@outlook.com>
2025-09-30 00:58:08 -0300

    Correção 2 do exercício a7

--- PHP-ARRAYS/lp-cod-php-a7.php
+++ PHP-ARRAYS/lp-cod-php-a7.php
@@ -14,31 +14,38 @@ function valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas)
     return $resultado;
 }
 
-for($i = 0; $i < $quantidadePessoas; $i++) {
-    echo "Pessoa: " . ($i+1) . " vai pagar R$ " . valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas) .PHP_EOL;
-}
-
 echo "".PHP_EOL;
 
-
-if($quantidadePessoas < 1){
+if($quantidadePessoas <= 1){
     echo "O churrasco foi cancelado, todo mundo furou!";
 
 } elseif ($precoItens[0] > $precoItens[1] && $precoItens[0] > $precoItens[2] && $precoItens[0] > $precoItens[3]){
+    for($i = 0; $i < $quantidadePessoas; $i++) {
+    echo "Pessoa " . ($i+1) . " vai pagar R$ " . valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas) .PHP_EOL;
+}
     echo "O item mais caro é: " .$itensChurrasco[0] .PHP_EOL;
     echo "Ela custa: R$ " . $precoItens[0] .PHP_EOL;
 
 } elseif ($precoItens[1] > $precoItens[0] && $precoItens[1] > $precoItens[2] && $precoItens[1] > $precoItens[3]) {
+    for($i = 0; $i < $quantidadePessoas; $i++) {
+    echo "Pessoa " . ($i+1) . " vai pagar R$ " . valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas) .PHP_EOL;
+}
     echo "O item mais caro é: " .$itensChurrasco[1] .PHP_EOL;
     echo "Ela custa: R$ " . $precoItens[1] .PHP_EOL;
 
 
 } elseif ($precoItens[2] > $precoItens[0] && $precoItens[2] > $precoItens[1] && $precoItens[2] > $precoItens[3]) {
+    for($i = 0; $i < $quantidadePessoas; $i++) {
+    echo "Pessoa " . ($i+1) . " vai pagar R$ " . valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas) .PHP_EOL;
+}
     echo "O item mais caro é: " .$itensChurrasco[2] .PHP_EOL;
     echo "Ela custa: R$ " . $precoItens[2] .PHP_EOL;
 
 
 } else {
+    for($i = 0; $i < $quantidadePessoas; $i++) {
+    echo "Pessoa " . ($i+1) . " vai pagar R$ " . valorDeCadaPessoa ($valorTotalItens,$quantidadePessoas) .PHP_EOL;
+}
     echo "O item mais caro é: " .$itensChurrasco[3];
     echo "Ela custa: R$ " . $precoItens[3] .PHP_EOL;
 }
